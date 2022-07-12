c69306b6f61096fe0fafe81e3497c76271924825
Adar Arbiv <mrmetrix4@gmail.com>
2022-07-13 01:45:46 +0300

    Added validators

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from structs.series import *
 from structs.board import *
 
 
+# TODO: tests
+
 def main():
-    c1 = Card(number=1, type=CardType.HEARTS)
-    s1 = Series(cards=[c1], type=SeriesType.COLOR)
+    c1 = Card(number=3, type=CardType.SPADES)
+    c2 = Card(number=4, type=CardType.SPADES)
+    c3 = Card(number=2, type=CardType.SPADES)
+    s1 = Series(type=SeriesType.STAIRS, cards=[c1, c2, c3])
     b1 = Board(series=[s1])
     print(b1)
 
--- structs/series.py
+++ structs/series.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import List
 from enum import Enum, auto
 
@@ -6,11 +6,31 @@ from structs.card import Card
 
 
 class SeriesType(Enum):
-    ASCENDING = auto()
-    DESCENDING = auto()
-    COLOR = auto()
+    STAIRS = auto()
+    TYPE = auto()
 
 
 class Series(BaseModel):
-    cards: List[Card]
     type: SeriesType
+    cards: List[Card]
+
+    @validator('cards')
+    def validate_cards(cls, v, values):
+        if len(v) < 3:
+            raise ValueError('Series must be at least 3 cards long')
+
+        series_type = values['type']
+        if series_type == SeriesType.TYPE:
+            if len(set(c.type for c in v)) != len(v):
+                raise ValueError('Type series can not contain the same type twice')
+            if not all(v[0].number == c.number for c in v[1:]):
+                raise ValueError('Type series can not contain different numbers')
+            return sorted(v, key=lambda c: c.type.value)
+        if series_type == SeriesType.STAIRS:
+            if not all(v[0].type == c.type for c in v[1:]):
+                raise ValueError('Type must be unique throughout a stairs series')
+            sorted_cards = sorted(v, key=lambda c: c.number)
+            sorted_numbers = [c.number for c in sorted_cards]
+            if sorted_numbers != list(range(sorted_numbers[0], sorted_numbers[-1] + 1)):
+                raise ValueError('Series is missing or doubling some steps')
+            return sorted_cards
